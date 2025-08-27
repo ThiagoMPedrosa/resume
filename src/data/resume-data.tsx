cb238891cb3b9d48c0ea0f5d2bd20bb400bbf91d
tpedrosa <thiago.pedrosa@thalesgroup.com>
2025-08-27 14:02:11 -0400

    Changing resume-data

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -9,232 +9,205 @@ import {
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  name: "Thiago Pedrosa",
+  initials: "TP",
+  location: "Ontario, Canada, EST",
+  locationLink: "https://www.google.com/maps/place/Ontario",
   about:
-    "Detail-oriented Full Stack Engineer dedicated to building high-quality products.",
+    "An accomplished software engineer with 8 years of growth and innovation, currently specializing in driving projects to success.",
   summary: (
     <>
-      Frontend-focused Full Stack Engineer specializing in high-performance
-      React applications, scalable Node.js services, and real-time collaboration
-      systems. Experienced in technical architecture design and remote team
-      leadership.
+      Backend-focused Full Stack Engineer specializing in Banking and Payment
+      applications, scalable Django services, and real-time collaboration
+      systems. Experienced in technical architecture design and remote deployments.
     </>
   ),
   avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+  personalWebsiteUrl: "https://notabug.dev",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "thiago.mp13@hotmail.com",
+    tel: "+3657790082",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/ThiagoMPedrosa",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/thiago-pedrosa-8a521b158/",
         icon: LinkedInIcon,
-      },
-      {
-        name: "X",
-        url: "https://x.com/BartoszJarocki",
-        icon: XIcon,
       },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "Faculdades Integradas Campos Salles",
+      degree: "Bachelor's Degree in Information Systems",
+      start: "2016",
+      end: "2019",
     },
   ],
   work: [
     {
-      company: "Film.io",
-      link: "https://film.io",
-      badges: ["Remote", "React", "Next.js", "TypeScript", "Node.js"],
-      title: "Software Architect",
-      logo: ConsultlyLogo,
-      start: "2024",
+      company: "Thales Cyber & Digital Canada",
+      link: "https://www.thalesgroup.com",
+      badges: ["Hybrid", "Project Management", "Python", "Django", "Java", "C#"],
+      title: "Industrial Personalization Solutions Field Support Senior",
+      //logo: ConsultlyLogo,
+      start: "2025",
       end: null,
       description: (
         <>
-          Leading technical architecture of a blockchain-based film funding
-          platform.
+          Responsible for implementing, maintaining, and enhancing Thales' 
+          personalization systems across the Americas region.
           <ul className="list-inside list-disc">
             <li>
-              Architecting migration from CRA to Next.js for improved
-              performance, SEO, and DX
+              Serve as subject matter expert in the development and deployment of Thales’ 
+              internal solutions, utilizing Java, Python, C#, and Velocity.
             </li>
             <li>
-              Established release process enabling faster deployments and
-              reliable rollbacks
+              Provide advanced technical support for multi-site projects across the Americas
+              ensuring seamless process implementation and effective integration of Thales solutions.
             </li>
             <li>
-              Implementing system-wide monitoring and security improvements
+              Implement, optimize, and maintain Thales Banking & Payment systems
+              across the Americas region, identifying improvement opportunities and ensuring
+              reliability through cloud-based monitoring tools.
             </li>
           </ul>
         </>
       ),
     },
     {
-      company: "Parabol",
-      link: "https://parabol.co",
-      badges: [
-        "Remote",
-        "React",
-        "TypeScript",
-        "Node.js",
-        "GraphQL",
-        "Tailwind CSS",
-      ],
-      title: "Senior Full Stack Developer",
-      logo: ParabolLogo,
-      start: "2021",
-      end: "2024",
+      company: "Thales Cyber & Digital Canada",
+      link: "https://www.thalesgroup.com",
+      badges: ["Hybrid", "Python", "Django", "Java", "C#", "Oracle"],
+      title: "Perso System Support Engineer Specialist",
+      //logo: ConsultlyLogo,
+      start: "2022",
+      end: "2025",
       description: (
         <>
-          Senior developer and squad leader for an enterprise agile meeting
-          platform.
+          Responsible for implementing, maintaining, and enhancing Thales' 
+          personalization systems across the Americas region.
           <ul className="list-inside list-disc">
             <li>
-              Built design system with Tailwind CSS, improving development speed
-              and time to market
+              Develop and deploy Thales' internal systems, focused on Java, Python, C#, and Velocity
             </li>
             <li>
-              Implemented engineering practices: PR automation, code review
-              guidelines, and workflows
+              Direct interface for local teams across the USA and Canada, ensuring
+              seamless processes implementation, promoting APS and other Thales’ solutions.
             </li>
             <li>
-              Open source contributions to Relay DevTools and React i18n tooling
+              Responsible for Canada’s Banking & Payment Infrastructure.
             </li>
+            <li>
+              Personalization, innovation, development, and security standards.
+            </li>
+            <li>
+              Troubleshoot data processing, adhering to ISO specifications, and support PCI-CP (VISA/MC) compliance.
+            </li>            
           </ul>
         </>
       ),
     },
     {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote", "React", "TypeScript", "Node.js", "Android", "Kotlin"],
-      title: "Lead Android Developer → Full Stack Developer",
-      logo: ClevertechLogo,
-      start: "2015",
-      end: "2021",
+      company: "Thales Cyber & Digital Brazil",
+      link: "https://www.thalesgroup.com",
+      badges: ["On Site", "JavaScript", "PHP", "EMV", "PLSQL", "Oracle", "SPI"],
+      title: "Order Engineering - Software Developer",
+      //logo: ConsultlyLogo,
+      start: "2019",
+      end: "2022",
       description: (
         <>
-          Successfully transitioned from mobile to full-stack development while
-          leading distributed teams.
+          Responsible for implementing, maintaining, and enhancing Thales' 
+          personalization systems across the Americas region.
           <ul className="list-inside list-disc">
             <li>
-              Led frontend team at Evercast, building real-time platform
-              supporting 30+ users per room with HD streaming and collaboration
-              tools
+              Software Development & Validation: data processing, Electrical Personalization (EMV), quality controls, and reports.
             </li>
             <li>
-              Developed offline-first Android app for DKMS, improving donor
-              registration process
-            </li>
-            <li>
-              Led development teams across multiple successful client projects
-            </li>
+              Creation & improvement of Data Processing tools focusing on performance, 
+              processing large amounts of data, advanced cryptography, and managing
+              data for utilization in banking Smartcards (EMV data preparation).
+            </li>       
           </ul>
         </>
       ),
     },
     {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
-      badges: ["On Site", "Android", "Java", "Kotlin"],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
+      company: "Thales Cyber & Digital Brazil",
+      link: "https://www.thalesgroup.com",
+      badges: ["On Site", "DoS", "EMV", "PLSQL", "Oracle"],
+      title: "New Product Integration Technician",
+      //logo: ConsultlyLogo,
+      start: "2016",
+      end: "2019",
       description: (
         <>
-          First Android developer, grew and led a team of 15+ engineers while
-          establishing engineering culture.
+          Responsible for implementing, maintaining, and enhancing Thales' 
+          personalization systems across the Americas region.
           <ul className="list-inside list-disc">
             <li>
-              Developed apps for major Polish companies including LOT, Polskie
-              Radio, and Agora
+              Definition and execution of new technologies, methods, processes, 
+              and product tests, implementation, and acceptance test of new banking
+              personalization machines.
             </li>
-            <li>Built and mentored high-performing mobile development team</li>
+            <li>
+              Definition and implementation of new methods, processes, and products.
+            </li>
+            <li>
+              Previous and post-implementation tests of new projects in production.
+            </li>
           </ul>
         </>
       ),
-    },
-    {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: ["On Site", "C/C++", "LTE", "Agile"],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description:
-        "Developed software for LTE base stations at enterprise scale, gaining strong fundamentals in software architecture, testing practices, and cross-team collaboration.",
     },
   ],
   skills: [
-    "React/Next.js/Remix",
-    "TypeScript",
-    "Tailwind CSS",
-    "Design Systems",
-    "WebRTC",
-    "WebSockets",
-    "Node.js",
-    "GraphQL",
-    "Relay",
+    "Django",
+    ".NET",
+    "Flask",
+    "Oracle, Mariadb, MySQL",
+    "Project Management",
+    "EMV Transactions and concepts",
+    "SQL and PLSQL",
+    "Python, JavaScript, HTML, CSS, PHP, Java, Velocity, C#.",
+    "Gitlab CI/CD",
+    "IIS, Nginx",
+    "AWS, Google Cloud, Datadog",
+    "Data Processing",
+    "Agile and Kanban methodologies",
     "System Architecture",
     "Remote Team Leadership",
   ],
   projects: [
     {
-      title: "Monito",
-      techStack: ["TypeScript", "Next.js", "Browser Extension", "PostgreSQL"],
+      title: "Inventory Management System",
+      techStack: ["Django", "Python", "MariaDB", "IIS, Nginx", "LDAP"],
       description:
-        "Browser extension for debugging web applications. Includes taking screenshots, screen recording, E2E tests generation and generating bug reports",
-      logo: MonitoLogo,
+        "Django Web Server application that manages Inventory stock and KPIs across multiple sites worldwide.",
+      //logo: MonitoLogo,
       link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
+        label: "IMS",
+        //href: "https://monito.dev/",
       },
     },
     {
-      title: "Consultly",
+      title: "Voluntariando",
       techStack: [
-        "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
-        "Tailwind CSS",
-        "PostgreSQL",
-        "Redis",
+        "Node.js",
+        "Next.Firebase",
+        "AWS S3",
       ],
       description:
-        "Platform for online consultations with real-time video meetings and scheduling",
-      logo: ConsultlyLogo,
+        "Improve the information access about voluntary positions, bringing position posts and their availability according to the filters.",
+      //logo: ConsultlyLogo,
       link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
-      },
-    },
-    {
-      title: "Minimalist CV",
-      techStack: ["TypeScript", "Next.js", "Tailwind CSS"],
-      description:
-        "An open source minimalist, print friendly CV template with a focus on readability and clean design. >9k stars on GitHub",
-      logo: MonitoLogo,
-      link: {
-        label: "Minimalist CV",
-        href: "https://github.com/BartoszJarocki/cv",
+        label: "voluntariando",
+        //href: "https://consultly.com/",
       },
     },
   ],
